b34c281028a084f109293887e587bc7442477dd7
EgonJanter <168183257+EgonJanter@users.noreply.github.com>
2024-04-26 14:28:31 +0300

    Update index.php
    muutsin pildi

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1>Meie projekti pealeht</h1>
     <p>See leht arvestab ka muudatustega</p>
-    <img src="https://shorturl.at/eBFN2"
+    <img src="https://th.bing.com/th/id/R.f6739d85be1b8ff4e32e9c78b96aee6a?rik=jvv8nwILpTs6mA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-wrGHPJCy4Z8%2fUH0yUwvmcnI%2fAAAAAAAAIMk%2fA6RGuOKkwZk%2fs1600%2fgirl-wallpaper-model-sexual-beautiful.-super.jpg&ehk=HEDim3wNnrUx4CMXzlbug31p7CkNPsvHaLMaUyNj%2fkY%3d&risl=&pid=ImgRaw&r=0"
   </body>
 </html>
